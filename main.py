70434ba1b0fa76d06aef83fdde9f8cf673bc1327
Cerek Hillen <cerekh@gmail.com>
2019-11-16 10:38:07 -0500

    Impl word vec distance stuff

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
+import lib.semantics as semantics
 
 
 def main(argv):
-    print("Hello {}".format(argv[1]))
+    word_similarity_map = semantics.TwitterGloveSimilarWordMap()
+    while True:
+        word = input("> ")
+        for similar in word_similarity_map.get_similar_words(word, 5):
+            print("    {}".format(similar))
 
 
 if __name__ == "__main__":
