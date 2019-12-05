efb9ed791cc03a1bbd4a943d1493a98d8de1e845
Cerek Hillen <cerekh@gmail.com>
2019-12-05 13:26:46 -0500

    GUI

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './images/logo.svg';
+import brain from './images/brain.svg';
 import './styles/App.css';
+
+const SERVER_HOST = 'localhost';
+const SERVER_PORT = 3000;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-section">
+        <div className="title">
+          <img className="brain" src={brain} />
+          DadBot
+        </div>
+        <div className="subtitle">Computationally Generated Puns</div>
+      </div>
+
+      <div className="contents">
+        <div>
+          <input type="text" placeholder="Topic" />
+        </div>
+
+        <div>
+          <input type="text" placeholder="Sentence" />
+        </div>
+
+        <div>
+          <button>Punnify</button>
+        </div>
+
+        <div>
+          <div className="more-like">more like...</div>
+          <input type="text" placeholder="Output" disabled="true" />
+        </div>
+      </div>
     </div>
   );
 }
